2073b8ae5186da688eac7a24554275aac9b8e0dd
Tina Huynh <tinamchuynh@gmail.com>
2024-08-23 20:52:37 -0700

    create routes

--- flask_app/controllers/general_controller.py
+++ flask_app/controllers/general_controller.py
@@ -7,17 +7,31 @@ bp = Blueprint('general', __name__)
 
 Session = sessionmaker(bind=engine)
 
-@bp.route("/upload", methods=['GET', 'POST'])
-def upload_file():
+@bp.route("/import", methods=['GET', 'POST'])
+def doimport():
     if request.method == 'POST':
-        return jsonify({"result": request.get_array(field_name='file')})
+        def author_init_func(row):
+            c = Author(row['username'])
+            c.email = row['email']
+            c.id = row['id']
+            return c
+
+        def book_init_func(row):
+            c = Author.query.filter_by(name=row['author']).first()
+            p = Book(row['title'], c)
+            return p
+        request.save_book_to_database(
+            field_name='file', session=db.session,
+            tables=[Book, Author],
+            initializers=[book_init_func, author_init_func])
+        return redirect(url_for('.handson_table'), code=302)
     return '''
     <!doctype html>
     <title>Upload an excel file</title>
-    <h1>Excel file upload (csv, tsv, csvz, tsvz only)</h1>
-    <form action="" method=post enctype=multipart/form-data><p>
+    <h1>Excel file upload (xls, xlsx, ods please)</h1>
+    <form action="" method=author enctype=multipart/form-data><p>
     <input type=file name=file><input type=submit value=Upload>
-    </form>F
+    </form>
     '''
 
 
@@ -25,17 +39,20 @@ def upload_file():
 def download_file():
     return excel.make_response_from_array([[1, 2], [3, 4]], "csv")
 
-
 @bp.route("/export", methods=['GET'])
-def export_records():
-    return excel.make_response_from_array([[1, 2], [3, 4]], "csv",
-                                          file_name="export_data")
+def doexport():
+    return excel.make_response_from_tables(db.session, [Author, Book], "xls")
 
-
-@bp.route("/download_file_named_in_unicode", methods=['GET'])
-def download_file_named_in_unicode():
-    return excel.make_response_from_array([[1, 2], [3, 4]], "csv",
-                                          file_name=u"中文文件名")
+@bp.route("/handson_view", methods=['GET'])
+def handson_table():
+    return excel.make_response_from_tables(
+        db.session, [Author, Book], 'handsontable.html')
+    
+@bp.route("/custom_export", methods=['GET'])
+def docustomexport():
+    query_sets = Category.query.filter_by(id=1).all()
+    column_names = ['author', 'title']
+    return excel.make_response_from_query_sets(query_sets, column_names, "xls")
 
 
 # insert database related code here
