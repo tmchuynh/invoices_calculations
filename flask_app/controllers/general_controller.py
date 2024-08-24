baf14ad09cdc60eb908f6f529bc39a3640554cff
Tina Huynh <tinamchuynh@gmail.com>
2024-08-24 16:09:12 -0700

    organize routes vs functions

--- flask_app/controllers/general_controller.py
+++ flask_app/controllers/general_controller.py
@@ -9,6 +9,70 @@ bp = Blueprint('general', __name__)
 
 df_global = None
 
+def read_excel(file):
+    df = pd.read_excel(file, engine='openpyxl', dtype=str)
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%Y-%m-%d %H:%M:%S')
+
+    # Reformat the 'Timestamp' column to the desired format
+    df['Timestamp'] = df['Timestamp'].dt.strftime('%b %d %y %I:%M:%S %p')
+    
+    df = convert_to_number(df)
+
+    return df
+
+
+def filter_by_month(df, column_name, month):
+    df[column_name] = pd.to_datetime(df[column_name], format='%b %d %y %I:%M:%S %p')
+    return df[df[column_name].dt.month == month]
+
+def filter_by_column(df, column_name, value):
+    if column_name in df.columns:
+        return df[df[column_name].str.contains(value, case=False, na=False)]
+    return df
+
+
+def addition(df, column_name):
+    
+
+
+def convert_to_number(df):
+    column_indices = [5, 6, 9, 10, 11, 12, 13, 14, 15, 16]
+    # Convert the specified columns to numeric
+    for col in column_indices:
+        # Access the column by its index and convert to numeric
+        df[df.columns[col]] = pd.to_numeric(df.iloc[:, col], errors='coerce')
+        df[df.columns[col]] = df[df.columns[col]].fillna(0).astype('Int64')
+    return df
+
+
+def format_currency(df):
+    currency_columns = ['Total $$ for the month', 'Did you work on any side projects?', 'Calculated Total Amount']
+    for col in currency_columns:
+        if col in df.columns:
+            df[col] = pd.to_numeric(df[col], errors='coerce')
+            df[col] = df[col].apply(lambda x: f"${x:,.2f}" if pd.notna(x) else "")
+    return df
+
+
+def sum_and_format_numbers(df, column_name):
+    # Function to extract numbers and clean text
+    def extract_and_sum_numbers(text):
+        # Extract numbers and remove them from text
+        numbers = re.findall(r'\d+\.?\d*', text)
+        # Convert extracted numbers to floats and sum them
+        numbers = map(float, numbers)
+        return sum(numbers)
+    
+    # Check if the specified column exists
+    if column_name in df.columns:
+        # Apply extraction and summing function
+        df[column_name] = df[column_name].apply(lambda x: extract_and_sum_numbers(x))
+        
+        # Convert the column to numeric and format as currency
+        df[column_name] = pd.to_numeric(df[column_name], errors='coerce').apply(lambda x: f"${x:,.2f}" if pd.notna(x) else "")
+    
+    return df
+
 @bp.route('/', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
@@ -23,6 +87,9 @@ def upload():
                 df = df.apply(lambda col: col.str.strip() if col.dtype == "object" else col)
                 df = df.fillna('')
                 df.insert(3, 'Calculated Total Amount', '300')
+                df.insert(9, 'Total # of Classes', '')
+                # df.insert(9, 'Receipt Expenses', ' ')
+                
 
                 global df_global
                 df_global = df
@@ -30,53 +97,6 @@ def upload():
                 return redirect('/results')
     return render_template('upload.html')
 
-def read_excel(file):
-    df = pd.read_excel(file, engine='openpyxl', dtype=str)
-    df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%Y-%m-%d %H:%M:%S')
-
-    # Reformat the 'Timestamp' column to the desired format
-    df['Timestamp'] = df['Timestamp'].dt.strftime('%b %d %y %I:%M:%S %p')
-    
-    df = convert_to_number(df)
-
-    return df
-
-def filter_by_month(df, timestamp_column, month):
-    df[timestamp_column] = pd.to_datetime(df[timestamp_column], format='%b %d %y %I:%M:%S %p')
-    return df[df[timestamp_column].dt.month == month]
-
-def filter_by_column(df, column_name, value):
-    if column_name in df.columns:
-        return df[df[column_name].str.contains(value, case=False, na=False)]
-    return df
-
-def convert_to_number(df):
-    column_indices = [5, 6, 9, 10, 11, 12, 13, 14, 15, 16]
-    # Convert the specified columns to numeric
-    for col in column_indices:
-        # Access the column by its index and convert to numeric
-        df[df.columns[col]] = pd.to_numeric(df.iloc[:, col], errors='coerce')
-        df[df.columns[col]] = df[df.columns[col]].fillna(0).astype('Int64')
-    return df
-
-def format_currency(df):
-    currency_columns = ['Total $$ for the month', 'Did you work on any side projects?', 'Calculated Total Amount']
-    for col in currency_columns:
-        if col in df.columns:
-            df[col] = pd.to_numeric(df[col], errors='coerce')
-            df[col] = df[col].apply(lambda x: f"${x:,.2f}" if pd.notna(x) else "")
-    return df
-
-def sum_and_format_numbers(df, column_name):
-    def extract_and_sum_numbers(text):
-        numbers = re.findall(r'\d+\.?\d*', text)
-        numbers = map(float, numbers)
-        return sum(numbers)
-    
-    if column_name in df.columns:
-        df.loc[:, column_name] = df[column_name].apply(lambda x: extract_and_sum_numbers(x))
-        df.loc[:, column_name] = df[column_name].apply(lambda x: f"${x:,.2f}" if pd.notna(x) else "")
-    return df
 
 @bp.route('/results', methods=['GET', 'POST'])
 def results():
@@ -99,6 +119,7 @@ def results():
                 df = filter_by_column(df, 'Full Name', name_filter)
             
             df = sum_and_format_numbers(df, 'Any invoices/receipts?')
+            df = convert_to_number(df)
             df = format_currency(df)
             
             # Convert to HTML table
@@ -113,6 +134,7 @@ def results():
             table_html = df.to_html(classes='table table-striped', index=False, na_rep='', max_rows=None, max_cols=None)
             return render_template('results.html', table_html=table_html, df_global=df)
     return redirect('/')
+
 
 @bp.route('/see_all', methods=['GET'])
 def see_all():
