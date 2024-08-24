ceff0f8632494dfb52cb7e28ba3a8d4864920c35
Tina Huynh <tinamchuynh@gmail.com>
2024-08-24 13:41:23 -0700

    formatting

--- flask_app/controllers/general_controller.py
+++ flask_app/controllers/general_controller.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, render_template, Blueprint, redirect
 import pandas as pd
 import re
+import xlwt
+import numpy as np
 
 # Create a Blueprint
 bp = Blueprint('general', __name__)
 
 df_global = None
 
-@bp.route('/upload', methods=['GET', 'POST'])
+@bp.route('/', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         if 'file' in request.files:
@@ -16,20 +18,26 @@ def upload():
                 df = read_excel(file)
                 df = df.apply(lambda col: col.str.strip() if col.dtype == "object" else col)
                 df = df.fillna('')
+                df.insert(3, 'Calculated Total Amount', '300')
 
                 # Store the DataFrame in the global variable
                 global df_global
                 df_global = df
-
+                
                 return redirect('/results')
     return render_template('upload.html')
 
 def read_excel(file):
     df = pd.read_excel(file, engine='openpyxl', dtype=str)
+    # df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%b %d %y %H:%M:%S')
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%Y-%m-%d %H:%M:%S')
+
+    # Reformat the 'Timestamp' column to the desired format
+    df['Timestamp'] = df['Timestamp'].dt.strftime('%b %d %y %I:%M:%S %p')
     return df
 
 def filter_by_month(df, timestamp_column, month):
-    df[timestamp_column] = pd.to_datetime(df[timestamp_column], format='%Y-%m-%d %H:%M:%S')
+    df[timestamp_column] = pd.to_datetime(df[timestamp_column], format='%b %d %y %I:%M:%S %p')
     return df[df[timestamp_column].dt.month == month]
 
 def filter_by_column(df, column_name, value):
@@ -81,15 +89,15 @@ def results():
 
             # Convert to HTML table
             table_html = df.to_html(classes='table table-striped', index=False, na_rep='', max_rows=None, max_cols=None)
-            return render_template('results.html', table_html=table_html)
+            return render_template('results.html', table_html=table_html, df_global=df_global)
 
         elif request.method == 'GET':
             # If GET request, show all data without filters
             df = sum_and_format_numbers(df, 'Any invoices/receipts?')
             df = format_currency(df)
             table_html = df.to_html(classes='table table-striped', index=False, na_rep='', max_rows=None, max_cols=None)
-            return render_template('results.html', table_html=table_html)
-    return redirect('/upload')
+            return render_template('results.html', table_html=table_html, df_global=df_global)
+    return redirect('/')
 
 @bp.route('/see_all', methods=['GET'])
 def see_all():
@@ -101,6 +109,6 @@ def see_all():
 
         # Render the modified DataFrame as HTML
         table_html = df.to_html(classes='table table-striped', index=False, na_rep='', max_rows=None, max_cols=None)
-        return render_template('results.html', table_html=table_html)
-    return redirect('/upload')
+        return render_template('results.html', table_html=table_html, df_global=df_global)
+    return redirect('/')
 
