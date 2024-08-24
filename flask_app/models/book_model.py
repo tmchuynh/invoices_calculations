d7d43b8135583ae40bd0fe650e380495fcc5a0c4
Tina Huynh <tinamchuynh@gmail.com>
2024-08-23 21:01:20 -0700

    thought i just did this

--- flask_app/models/book_model.py
+++ flask_app/models/book_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from flask_app.models.base_model import Base
 from sqlalchemy import String, DateTime, func, PrimaryKeyConstraint, ForeignKey
 
